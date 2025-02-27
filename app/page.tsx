f5f0d47df2550d338e34e196e2d4cc607a64917c
mejokkurian_2 <mejo.kk@vofoxsolutions.com>
2025-02-27 12:22:41 +0530

    pages updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import PlatformsSupported from "@/components/About/PlatformsSupported";
-import Blog from "@/components/Blog";
 import LogoSlider from "@/components/LogoSlider";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
 import KioskFeatures from "@/components/KioskFeatures";
 import KioskModes from "@/components/KioskModes";
-import Pricing from "@/components/Pricing";
 import SignupSection from "@/components/SignUpSection";
 import CustomerReviews from "@/components/CustomerReviews";
 import { Metadata } from "next";
